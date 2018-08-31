984e29dd3f8c8388191f48e470a01c84a4c9fcda
Dmitriy Beliaev <admin@cogear.ru>
2018-08-31 19:02:54 +0300

    Update to match new system version

--- __tests__/plugin.js
+++ __tests__/plugin.js
@@ -1,28 +1,27 @@
 
+const Emittery = require('emittery')
 let plugin = require('../plugin');
 
+class Cogear extends Emittery {
+	constructor(){
+		super()
+		this.config = {
+			youtube: {
+				width: 640,
+				height: 480
+			}
+		}
+	}
+}
 describe('cogear-plugin-youtube',()=>{
 	beforeEach(()=>{
 		this.parser = {
 			file: "index.md",
 			content: `!https://www.youtube.com/watch?v=cFh-cQelKPU`
 		}
+		global.cogear = new Cogear()
 		let parser = this.parser
-		global.cogear = {
-			config: {
-				youtube: {
-					width: 640,
-					height: 480
-				}
-			},
-			hooks: {
-				beforeParse: {
-					tap(name,callback){
-						callback(parser)
-					}
-				}
-			}
-	}})
+	})
 	test("it must parse youtube link",()=>{
 		plugin.apply()		
 		expect(this.parser.content).toContain('iframe')
