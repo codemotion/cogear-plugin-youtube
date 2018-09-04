afe7407fbec8ee60823b16292393d6734f0303d6
Dmitriy Beliaev <admin@cogear.ru>
2018-09-04 13:22:58 +0300

    Linting

--- plugin.js
+++ plugin.js
@@ -1,14 +1,14 @@
 module.exports = {
-		apply(){
-			// Process hooks here
-			cogear.on('parse.before',(parser)=>{
-				// If it's not a layout, but a page
-				if(parser.content && parser.content.indexOf('youtu') !== -1){
-					cogear.config.youtube = cogear.config.youtube || {}
-					width = cogear.config.youtube.width || 560
-					height = cogear.config.youtube.height || 315
-					parser.content= parser.content.replace(/(?:!)https?:\/\/(?:www\.)?youtu(?:\.be\/|be\.com\/watch\?v=)([\w_-]{11})/g,`<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/$1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`)
-				}
-			})
-		}
-}
+  apply(){
+    // Process hooks here
+    cogear.on('parse.before',(parser)=>{
+      // If it's not a layout, but a page
+      if(parser.content && parser.content.indexOf('youtu') !== -1){
+        cogear.config.youtube = cogear.config.youtube || {};
+        let width = cogear.config.youtube.width || 560;
+        let height = cogear.config.youtube.height || 315;
+        parser.content= parser.content.replace(/(?:!)https?:\/\/(?:www\.)?youtu(?:\.be\/|be\.com\/watch\?v=)([\w_-]{11})/g,`<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/$1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
+      }
+    });
+  }
+};
