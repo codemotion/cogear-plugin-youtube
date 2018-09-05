51c4e5c315afd96dd1a384cbd2b99a4554b453ad
Dmitriy Beliaev <admin@cogear.ru>
2018-09-05 21:24:25 +0300

    Add option for parsed string to process query params to a video player

--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@ module.exports = {
         cogear.config.youtube = cogear.config.youtube || {};
         let width = cogear.config.youtube.width || 560;
         let height = cogear.config.youtube.height || 315;
-        parser.content= parser.content.replace(/(?:!)https?:\/\/(?:www\.)?youtu(?:\.be\/|be\.com\/watch\?v=)([\w_-]{11})/g,`<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/$1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
+        parser.content= parser.content.replace(/(?:!)https?:\/\/(?:www\.)?youtu(?:\.be\/|be\.com\/watch\?v=)([\w_-]{11})(\?.+)?/g,`<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/$1$2" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
       }
     });
   }
